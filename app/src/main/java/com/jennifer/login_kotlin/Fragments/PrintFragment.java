5cbe9a64fe904d5c9b0d648e96259ff8f933b9f1
jennifer <jennifertorrespalomino@gmail.com>
2023-10-16 20:23:39 -0500

    nueva actualizacion

---
+++ app/src/main/java/com/jennifer/login_kotlin/Fragments/PrintFragment.java
@@ -0,0 +1,20 @@
+package com.jennifer.login_kotlin.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jennifer.login_kotlin.R;
+public class PrintFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_print, container, false);
+    }
+}
--- app/src/main/java/com/jennifer/login_kotlin/MainActivity.kt
+++ app/src/main/java/com/jennifer/login_kotlin/MainActivity.kt
@@ -2,55 +2,87 @@ package com.jennifer.login_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.jennifer.login_kotlin.Fragments.CategoryFragment
+import com.jennifer.login_kotlin.Fragments.CustomerFragment
 import com.jennifer.login_kotlin.Fragments.FavoriteFragment
 import com.jennifer.login_kotlin.Fragments.HomeFragment
+import com.jennifer.login_kotlin.Fragments.PrintFragment
 import com.jennifer.login_kotlin.Fragments.SearchFragment
 import com.jennifer.login_kotlin.Fragments.UserFragment
 import com.jennifer.login_kotlin.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var fragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNavigationView.setOnItemSelectedListener { menuItem ->
-            when(menuItem.itemId){
-                R.id.bottom_home -> {
-                    replaceFragment(HomeFragment())
-                    true
-                }
-                R.id.bottom_search -> {
-                    replaceFragment(SearchFragment())
-                    true
-                }
-                R.id.bottom_add -> {
-                    Toast.makeText(this, "Add new product" , Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.bottom_favorite -> {
-                    replaceFragment(FavoriteFragment())
-                    true
-                }
-                R.id.bottom_user -> {
-                    replaceFragment(UserFragment())
-                    true
-                }
-                else -> false
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.open_nav, R.string.close_nav)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_search -> openFragment(SearchFragment())
+                R.id.bottom_favorite -> openFragment(FavoriteFragment())
+                R.id.bottom_user-> openFragment(UserFragment())
             }
+            true
         }
-        replaceFragment(HomeFragment())
+
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+
+        binding.fab.setOnClickListener{
+            Toast.makeText(this, "add", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
-    private fun replaceFragment(fragment: Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_home -> openFragment(HomeFragment())
+            R.id.nav_category -> openFragment(CategoryFragment())
+            R.id.nav_customer -> openFragment(CustomerFragment())
+            R.id.nav_print -> openFragment(PrintFragment())
+            R.id.nav_logout -> Toast.makeText(this,"Logout", Toast.LENGTH_SHORT).show()
+
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else {
+            super.getOnBackPressedDispatcher().onBackPressed()
+        }
+        super.onBackPressed()
+    }
+    private fun openFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
